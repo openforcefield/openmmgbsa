924e6e21954c15d7f8a8e1e2afdc33e27f5da5ea
Jaime RGP <jaime.rogue@gmail.com>
2019-11-11 17:59:18 +0100

    Add first skel draft and a dummy CLI app

---
+++ openmmgbsa/parametrization.py
@@ -0,0 +1,18 @@
+"""
+Parametrization and typing helpers
+"""
+
+class BaseParametrizer:
+    pass
+
+
+class ReceptorParametrizer(BaseParametrizer):
+    pass
+
+
+class LigandParametrizer(BaseParametrizer):
+    pass
+
+
+def build_system():
+    pass
